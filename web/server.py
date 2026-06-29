888155cfbc059656b685e0be0830d63e8697d9ca
Oscurin <dominicoscuro@hotmail.com>
2026-06-28 20:36:34 -0600

    server certificates

--- web/server.py
+++ web/server.py
@@ -8,12 +8,21 @@ logger = logging.getLogger("web.server")
 
 class WebServer:
     def __init__(self, app, host: str = "0.0.0.0", port: int = 5058):
+        current_dir = pathlib.Path(__file__).parent.resolve()
+        cert_path = current_dir / "config" / "cert.pem"
+        key_path = current_dir / "config" / "key.pem"
+        
+        ssl_cert = str(cert_path) if cert_path.exists() else None
+        ssl_key = str(key_path) if key_path.exists() else None
+        
         self.config = uvicorn.Config(
             app,
             host=host,
             port=port,
             log_level="info",
-            loop="asyncio"
+            loop="asyncio",
+            ssl_certfile=ssl_cert,
+            ssl_keyfile=ssl_key
         )
         self.server = uvicorn.Server(self.config)
         self.task = None
@@ -59,7 +68,8 @@ class WebServer:
             l.propagate = False
 
     def start(self):
-        logger.info(f"🌐 Iniciando servidor web en http://{self.config.host}:{self.config.port}...")
+        protocol = "https" if self.config.ssl_keyfile and self.config.ssl_certfile else "http"
+        logger.info(f"🌐 Iniciando servidor web en {protocol}://{self.config.host}:{self.config.port}...")
         self.task = asyncio.create_task(self.server.serve())
 
     async def stop(self):
